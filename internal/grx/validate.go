11c1e72b6a923fa743f2c2fb887cfc1f1eea4d8a
Carlos Castro <carlos_o_castro@yahoo.com.ar>
2019-08-15 20:32:39 -0300

    Primer Commit

--- internal/grx/validate.go
+++ internal/grx/validate.go
@@ -18,13 +18,13 @@ func validate(arr []string, reg string, min, max int64) bool {
 
 	for i := min - 1000; i <= max+1000; i++ {
 		str := fmt.Sprintf("%d", i)
-		esta := false
+		found := false
 		for _, b := range arr {
 			if str == strings.Trim(b, " ") {
-				esta = true
+				found = true
 			}
 		}
-		if !esta {
+		if !found {
 			matched, err := regexp.Match(reg, []byte(strings.Trim(str, " ")))
 			if matched {
 				fmt.Println("Match ERROR", str, err)
