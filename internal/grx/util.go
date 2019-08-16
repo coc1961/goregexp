28db4dec435749e989703e8509245725f2ae3319
Carlos Castro <carlos_o_castro@yahoo.com.ar>
2019-08-16 19:26:54 -0300

    Add Error Messages

---
+++ internal/grx/util.go
@@ -0,0 +1,15 @@
+package grx
+
+func removeDuplicates(elements []string) []string {
+	encountered := map[string]bool{}
+	result := []string{}
+
+	for v := range elements {
+		if encountered[elements[v]] == true {
+		} else {
+			encountered[elements[v]] = true
+			result = append(result, elements[v])
+		}
+	}
+	return result
+}
