b1156d832d06f77659e66fb6a46e2c824c077835
Carlos Castro <carlos_o_castro@yahoo.com.ar>
2019-08-16 19:23:37 -0300

    Add Error Messages

--- main.go
+++ main.go
@@ -5,16 +5,32 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/coc1961/goregexp/internal/grx"
 )
 
 func main() {
 
+	start := false
+
+	go func(start *bool) {
+
+		<-time.After(time.Second * 3)
+		if !*start {
+			fmt.Println("I can't read the Numbers List from stdin")
+			os.Exit(1)
+		}
+
+	}(&start)
+
 	data := make([]string, 0)
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		text, _, err := reader.ReadLine()
+		if !start {
+			start = true
+		}
 		if err != nil {
 			break
 		}
